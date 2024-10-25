62f5bf55a766b04e2a8b25a8827ec268cf05f2ff
Jake Perkins <jake.perkins@consensys.net>
2024-10-25 15:12:05 -0500

    hook up ddb

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
 import { getInput, setFailed, setOutput } from '@actions/core';
 import { context, getOctokit } from '@actions/github';
+import { Storage } from './storage';
 
 async function main() {
+
   try {
+
     const accessToken = getInput('github-token');
+    const tableName = getInput('build-version-table-name');
+
     const { owner, repo } = context.repo;
+
+    const githubContext = {
+      eventName: context.eventName,
+      sha: context.sha,
+      ref: context.ref,
+      actor: context.actor,
+      runNumber: context.runNumber,
+      runId: context.runId
+    };
 
     // Url is taken based on GITHUB_API_URL
     const client = getOctokit(accessToken);
@@ -16,7 +30,44 @@ async function main() {
     console.log(`Build version: ${buildVersion}`);
 
 
-    setOutput('build-version',buildVersion);
+
+
+    const storage = new Storage(tableName);
+
+    // Attempt to get the current version
+    const currentVersion = await storage.getCurrentVersion();
+    console.log(`Current version number retrieved: ${currentVersion.version}`);
+    console.log(`Last Updated at : ${currentVersion.updatedAt}`);
+    console.log(`Created By : ${currentVersion.githubContext.actor}`);
+    console.log(`For Github Ref : ${currentVersion.githubContext.ref}`);
+    console.log(`For Github SHA : ${currentVersion.githubContext.sha}`);
+    console.log(`For Github Run ID : ${currentVersion.githubContext.runId}`);
+    console.log(`For Github Run Number : ${currentVersion.githubContext.runNumber}`);
+    console.log(`For Github Event Name : ${currentVersion.githubContext.eventName}`);
+
+
+    const newVersion = currentVersion
+
+    //Increment Version & Meta Data
+    newVersion.version = currentVersion.version + 1;
+    newVersion.updatedAt = new Date().toISOString();
+    newVersion.githubContext = githubContext;
+    newVersion.versionKey = 'metamask-mobile';
+
+    const updatedVersion = await storage.updateVersion(newVersion);
+
+    console.log(`Updated version number: ${updatedVersion.version}`);
+    console.log(`Last Updated at : ${updatedVersion.updatedAt}`);
+    console.log(`Created By : ${updatedVersion.githubContext.actor}`);
+    console.log(`For Github Ref : ${updatedVersion.githubContext.ref}`);
+    console.log(`For Github SHA : ${updatedVersion.githubContext.sha}`);
+    console.log(`For Github Run ID : ${updatedVersion.githubContext.runId}`);
+    console.log(`For Github Run Number : ${updatedVersion.githubContext.runNumber}`);
+    console.log(`For Github Event Name : ${updatedVersion.githubContext.eventName}`);
+
+
+
+    setOutput('build-version', updatedVersion.version.toString());
 
   } catch (error) {
     const reason =
---
+++ src/types.ts
@@ -0,0 +1,52 @@
+import { DynamoDB } from "aws-sdk";
+
+export class GitHubContext {
+    eventName: string;
+    sha: string;
+    ref: string;
+    actor: string;
+    runNumber: number;
+    runId: number;
+
+    constructor(eventName: string, sha: string, ref: string, actor: string, runNumber: number, runId: number) {
+        this.eventName = eventName;
+        this.sha = sha;
+        this.ref = ref;
+        this.actor = actor;
+        this.runNumber = runNumber;
+        this.runId = runId;
+    }
+}
+
+export class BuildVersion {
+    versionKey: string;
+    version: number;
+    updatedAt: string;
+    githubContext: GitHubContext;
+
+    constructor(versionKey: string, version: number, updatedAt: string, context: GitHubContext) {
+        this.versionKey = versionKey;
+        this.version = version;
+        this.updatedAt = updatedAt;
+        this.githubContext = context;
+    }
+
+    toDynamoDBRecord(): DynamoDB.DocumentClient.AttributeMap {
+        return {
+            versionKey: this.versionKey,
+            version: this.version,
+            createdAt: this.updatedAt,
+            githubContext: this.githubContext
+        };
+    }
+
+    static fromDynamoDBRecord(record: DynamoDB.DocumentClient.AttributeMap): BuildVersion {
+
+        return new BuildVersion(
+            record.versionKey as string,
+            record.version as number,
+            record.createdAt as string,
+            record.githubContext as GitHubContext
+        );
+    }
+}
